0d80a0a7fa249702bd36e07d39e6ebb955deefcb
nimajam41 <nimxj4141@gmail.com>
2018-12-14 00:43:58 +0330

    Dog and Cat added

--- src/Cat.java
+++ src/Cat.java
@@ -1,11 +1,35 @@
-
-
-public class Cat extends DomesticAnimal implements Upgradable{
+public class Cat extends DomesticAnimal implements Upgradable {
     @Override
     public void upgrade() {
 
     }
-    private int level ;
-    private int maxLevel ;
 
+    private int level;
+    private int maxLevel;
+
+    @Override
+    public void randomWalk() {
+        super.randomWalk();
+    }
+
+    @Override
+    public void smartWalk() {
+        super.smartWalk();
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getMaxLevel() {
+        return maxLevel;
+    }
+
+    public void setMaxLevel(int maxLevel) {
+        this.maxLevel = maxLevel;
+    }
 }
