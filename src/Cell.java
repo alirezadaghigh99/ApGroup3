ee13e7c133c34683e10413c5f3807ac82a1e5d61
nimajam41 <nimxj4141@gmail.com>
2018-12-14 00:29:17 +0330

    cell added

--- src/Cell.java
+++ src/Cell.java
@@ -1,9 +1,30 @@
 import java.util.ArrayList;
 
 public class Cell {
-   private ArrayList onGroundThings = new ArrayList();
-   private ArrayList onAirThings = new ArrayList();
-   private Grass grass = new Grass();
+   private ArrayList<Animal> cellAnimals = new ArrayList<>();
+   private ArrayList<Product> cellProducts = new ArrayList<>();
+   private Grass grass;
 
+   public ArrayList<Animal> getCellAnimals() {
+      return cellAnimals;
+   }
+
+   public void addCellAnimals(Animal animal) {
+      this.cellAnimals.add(animal);
+   }
+
+   public ArrayList<Product> getCellProducts() {
+      return cellProducts;
+   }
+
+   public void addCellProducts(Product product) {
+      this.cellProducts.add(product);
+   }
+
+   public Grass getGrass() {
+      return grass;
+   }
 
 }
+
+
