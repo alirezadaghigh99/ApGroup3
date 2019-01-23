0f1c46779008fced7b9dfaa13f5dd9081f2bdf49
nimajam41 <nimxj4141@gmail.com>
2019-01-23 10:37:06 +0330

    menu

---
+++ src/View/GUI.java
@@ -0,0 +1,203 @@
+package View;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
+public class GUI extends Application {
+    private Group group = new Group();
+    private Scene scene = new Scene(group, 1018.5, 745.5);
+    private ImageView background;
+    private ImageView firstMenu;
+    private ImageView newGameButton;
+    private ImageView loadGameButton;
+    private ArrayList<Text> players = new ArrayList<>();
+
+    {
+        try {
+            FileInputStream bg = new FileInputStream("pictures\\bg.jpg");
+            Image image = new Image(bg);
+            background = new ImageView(image);
+            FileInputStream menu = new FileInputStream("pictures\\index.jpg");
+            Image image1 = new Image(menu);
+            firstMenu = new ImageView(image1);
+            FileInputStream buttonOfMenu = new FileInputStream("pictures\\aquabutton.jpg");
+            Image image2 = new Image(buttonOfMenu);
+            newGameButton = new ImageView(image2);
+            FileInputStream loadGame = new FileInputStream("pictures\\load.jpg");
+            Image lPic = new Image(loadGame);
+            loadGameButton = new ImageView(lPic);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initialize(ImageView background, ImageView firstMenu, ImageView newGameButton, ImageView loadGameButton) {
+        background.setX(0);
+        background.setY(0);
+        background.setFitHeight(scene.getHeight());
+        background.setFitWidth(scene.getWidth());
+        group.getChildren().add(background);
+        firstMenu.setX(300);
+        firstMenu.setY(175);
+        firstMenu.setFitHeight(400);
+        firstMenu.setFitWidth(400);
+        group.getChildren().add(firstMenu);
+        newGameButton.setX(400);
+        newGameButton.setY(225);
+        newGameButton.setFitWidth(200);
+        newGameButton.setFitHeight(100);
+        group.getChildren().add(newGameButton);
+        loadGameButton.setX(400);
+        loadGameButton.setY(425);
+        loadGameButton.setFitWidth(200);
+        loadGameButton.setFitHeight(100);
+        group.getChildren().add(loadGameButton);
+        newGameButton.setOnMouseClicked(event -> {
+            try {
+                newGameCall(firstMenu, newGameButton, loadGameButton);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void newGameCall(ImageView menu, ImageView newGameButton, ImageView loadGameButton) throws Exception {
+        group.getChildren().removeAll(menu, newGameButton, loadGameButton);
+        FileInputStream f = new FileInputStream("pictures\\secondMenu.jpg");
+        Image newGameImg = new Image(f);
+        ImageView newGameMenu = new ImageView(newGameImg);
+        newGameMenu.setX(150);
+        newGameMenu.setY(50);
+        newGameMenu.setFitHeight(650);
+        newGameMenu.setFitWidth(700);
+        group.getChildren().add(newGameMenu);
+        FileInputStream back = new FileInputStream("pictures\\backbutton2.png");
+        Image image = new Image(back);
+        ImageView backButton = new ImageView(image);
+        backButton.setX(200);
+        backButton.setY(600);
+        backButton.setFitWidth(100);
+        backButton.setFitHeight(80);
+        group.getChildren().add(backButton);
+        Text text = new Text();
+        text.setText("YOUR NAME :");
+        text.setFont(Font.font("elephant", 30));
+        text.setFill(Color.YELLOWGREEN);
+        text.setX(240);
+        text.setY(175);
+        group.getChildren().add(text);
+        TextField textField = new TextField();
+        textField.relocate(500, 150);
+        group.getChildren().add(textField);
+        textField.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.ENTER)) {
+                String name = textField.getText();
+                textField.clear();
+                if (name.equals("")) {
+
+                } else {
+                    boolean wasInPlayers = false;
+                    if (players.isEmpty()) {
+                        wasInPlayers = false;
+                    }
+                    for (Text player : players) {
+                        if (player.getText().equals(name)) {
+                            wasInPlayers = true;
+                            break;
+                        }
+                    }
+                    if (!wasInPlayers) {
+                        Text newPlayer = new Text();
+                        newPlayer.setText(name);
+                        players.add(newPlayer);
+                    }
+                }
+            }
+        });
+        FileInputStream playFile = new FileInputStream("pictures\\play.png");
+        Image playIMG = new Image(playFile);
+        ImageView playButton = new ImageView(playIMG);
+        playButton.setX(400);
+        playButton.setY(300);
+        playButton.setFitWidth(200);
+        playButton.setFitHeight(100);
+        group.getChildren().addAll(playButton);
+        FileInputStream showFile = new FileInputStream("pictures\\show.png");
+        Image showIMG = new Image(showFile);
+        ImageView showPlayersButton = new ImageView(showIMG);
+        showPlayersButton.setX(700);
+        showPlayersButton.setY(600);
+        showPlayersButton.setFitWidth(150);
+        showPlayersButton.setFitHeight(100);
+        group.getChildren().addAll(showPlayersButton);
+        playButton.setOnMouseClicked(event -> {
+
+        });
+        showPlayersButton.setOnMouseClicked(event -> {
+            try {
+                group.getChildren().removeAll(newGameButton, backButton, showPlayersButton, textField, playButton);
+                showPlayers();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        backButton.setOnMouseClicked(event -> {
+            group.getChildren().removeAll(group.getChildren());
+            initialize(background, firstMenu, newGameButton, loadGameButton);
+        });
+    }
+
+
+    public void loadGameCall(ImageView menu, ImageView newGameButton, ImageView loadGameButton) throws Exception {
+        group.getChildren().removeAll(menu, newGameButton, loadGameButton);
+        FileInputStream f = new FileInputStream("pictures\\secondMenu.jpg");
+        Image newGameImg = new Image(f);
+        ImageView newGameMenu = new ImageView(newGameImg);
+        newGameMenu.setX(150);
+        newGameMenu.setY(50);
+        newGameMenu.setFitHeight(650);
+        newGameMenu.setFitWidth(700);
+        group.getChildren().add(newGameMenu);
+    }
+
+    public void showPlayers() throws Exception {
+        FileInputStream f = new FileInputStream("pictures\\secondMenu.jpg");
+        Image playersIMG = new Image(f);
+        ImageView playersInGame = new ImageView(playersIMG);
+        playersInGame.setX(150);
+        playersInGame.setY(50);
+        playersInGame.setFitHeight(650);
+        playersInGame.setFitWidth(700);
+        group.getChildren().add(playersInGame);
+        Text[] texts = new Text[players.size() < 20 ? players.size() : 20];
+        int count = 1;
+        for (int i = players.size() - 1; i >= players.size() - (players.size() < 20 ? players.size() : 20); i--) {
+            texts[count - 1] = new Text();
+            texts[count - 1].setText(count + "- " + players.get(i).getText());
+            texts[count - 1].relocate(200, 50 + count * 50);
+            texts[count - 1].setFont(Font.font("comic sans ms", 30));
+            count++;
+        }
+        group.getChildren().addAll(texts);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Farm Frenzy");
+        initialize(background, firstMenu, newGameButton, loadGameButton);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
